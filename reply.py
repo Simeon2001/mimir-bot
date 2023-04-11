3e0d8974f84761e2532cfbc76987158d57e36b64
Simeon2001 <jesusanyasimeon@gmail.com>
2023-04-11 08:01:44 +0100

    updated to store last convo

--- reply.py
+++ reply.py
@@ -1,4 +1,3 @@
-from urllib import response
 import openai
 import os
 from dotenv import load_dotenv
@@ -7,20 +6,23 @@ load_dotenv()
 
 openai.api_key = os.getenv("GPT_KEY")
 
-
+messages=[]
+messages.append( {"role": "system", "content": "you are a bot named mimir, you are designed to provide people with intelligent and insigntful responses to questions and requests."})
 def answer(msg):
     try:
+        messages.append(
+                {"role": "user", "content": msg}
+            )
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "assistant", "content": msg}
-            ]
+            messages= messages
         )
 
         response = (completion.choices[0].message)
-        
+        messages.append({"role": "assistant", "content": response["content"]})
         return response["content"]
     
     except:
         msg = "my brain is currently snoozing"
         return msg
+
